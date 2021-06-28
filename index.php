c4e208b1dfb33d146afaaf04907bb2c23b1aa868
Đỗ Văn Huân <huanyd1@gmail.com>
2021-06-28 21:43:05 +0700

    --Update

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Đỗ Văn Huân</title>
+    <link rel="stylesheet"  href="css/home.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="img">
+            <img src="photosmile.vn_.jpg" alt="logo">
+        </div>
+        <h1>Đỗ Văn Huân</h1>
+        <p>Programmer - IT</p>
+        <div class="btn">
+            <div class="div_btn">
+                <img src="contact.png" alt="logo">
+                <a class="btn_is" href="tel://0983605825">Contact</a>
+            </div>
+            <div class="div_btn">
+                <img src="face.png" alt="logo">
+                <a class="btn_face" href="https://www.facebook.com/dohuanyd1" data-html="https://www.facebook.com/dohuanyd1">Facebook</a>
+            </div>
+            <div class="div_btn">
+                <img src="is.png" alt="logo">
+                <a class="btn_is" href="https://www.instagram.com/dohuan0905/" data-html="https://www.instagram.com/dohuan0905/">Instagram</a>
+            </div>
+            <div class="div_btn">
+                <img src="pinterest.png" alt="logo">
+                <a class="btn_yt" href="https://www.pinterest.com/huanyd1/" data-html="https://www.pinterest.com/huanyd1/">Pinterest</a>
+            </div>
+            <div class="div_btn">
+                <img src="github-logo.png" alt="logo">
+                <a class="btn_yt" href="https://github.com/huanyd1" data-html="https://github.com/huanyd1">Github</a>
+            </div>
+
+
+
+
+
+
+        </div>
+    </div>
+</body>
+</html>
